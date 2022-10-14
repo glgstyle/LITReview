e765eabc63938e41e2fec43cf79cab452950a992
glgstyle <gleguenno@gmail.com>
2022-10-14 21:37:49 +0200

    Adding style and html to create review page

--- review/views.py
+++ review/views.py
@@ -18,11 +18,15 @@ def createTicket(request):
 
 def createReview(request):
     """View function for createReview page of application."""
-    return HttpResponse("<h1>Page créer une critique (pas en rapport avec un ticket)</h1>")
+    # return HttpResponse("<h1>Page créer une critique (pas en rapport avec un ticket)</h1>")
+    return render(request, "create-review.html")
+
 
 def createReviewFromTicket(request):
     """View function for createReviewFromTicket page of application."""
-    return HttpResponse("<h1>Page créer une critique (en reponse à un ticket)</h1>")
+    # return HttpResponse("<h1>Page créer une critique (en reponse à un ticket)</h1>")
+    return render(request, "create-review-from-ticket.html")
+
     
 def subscription(request):
     """View function for subscription page of application."""
