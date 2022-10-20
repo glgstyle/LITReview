7e550b8bd7f8d7387c4ff9069193e644c397a96f
glgstyle <gleguenno@gmail.com>
2022-10-20 15:10:35 +0200

    Display alert message if user already exists in database before to save it

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from .registrationForm import NewUserForm
+from django.contrib.auth import authenticate
+from django.contrib import messages
 
 def home(request):
     """View function for home page of application."""
@@ -22,16 +24,22 @@ def registration(request):
             password = form.cleaned_data.get('your_password')
             user = User()
             user.username=email
-            # encrypt password
-            user.set_password(password)
-            user.save()
+            usernames = User.objects.filter(is_active=True).values_list('username', flat=True)
+            if email in usernames:
+                messages.error(request, 'Désolé ce mail existe déjà.', extra_tags='name')
+                # return redirect('/registration/')
+            else:
+                # encrypt password
+                user.set_password(password)
+                user.save()
+                # redirect to a new URL:
+                return HttpResponseRedirect('/registration-confirmation/')
             # user = authenticate(username=username, password=raw_password)
             # login(request, user)
             # return redirect('home')
             # process the data in form.cleaned_data as required
             # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/registration-confirmation/')
+
             # renvoyer vers un lien pour se connecter
 
     # if a GET (or any other method) we'll create a blank form
@@ -39,6 +47,16 @@ def registration(request):
         form = NewUserForm()
 
     return render(request, 'registration.html', {'form': form})
+    
+def login():
+    user = authenticate(username='john', password='secret')
+    if user is not None:
+        # A backend authenticated the credentials
+        pass
+    else:
+        pass
+        # No backend authenticated the credentials
+
 
 # fonction login 
 # si l'utilisateur existe et que sont mot de passe est correct log le sur flow/
